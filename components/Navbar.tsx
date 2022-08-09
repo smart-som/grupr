d04de854b1c7553fc8f036a48fe03add8d6406c3
Chibuike Nnaji <chibuikennaji306@gmail.com>
2022-08-09 20:01:15 +0100

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,40 +1,18 @@
 import React, { useState } from "react";
 import logo from "../images/logo-dark.svg";
 import Image from "next/image";
-import { FiMenu } from "react-icons/fi";
+import { CgMenu } from "react-icons/cg";
 import { MdOutlineClose } from "react-icons/md";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
 function Navbar() {
-  const router = useRouter();
-
-  const navLinks = [
-    {
-      title: "Home",
-      path: "/",
-    },
-    {
-      title: "Features",
-      path: "#features",
-    },
-    {
-      title: "Api",
-      path: "/api",
-    },
-    {
-      title: "Sign in",
-      path: "/signin",
-    },
-  ];
-
   const [isSidebar, setIsSidebar] = useState(false);
-
   return (
-    <header className="shadow-xs max-h-24 border-b border-b-purple-200/5 overflow-hidden w-full text-zinc-600 sticky top-0 bg-white/80 backdrop-blur-md  z-50">
-      <nav className=" max-w-6xl mx-auto flex items-center justify-between px-5 md:px-3 ">
-        <Link href="/" passHref>
-          <a className="w-24 h-16 relative cursor-pointer block">
+    <header className="shadow-xs max-h-24 border-b border-b-purple-200/5  w-full text-zinc-600 sticky top-0 bg-white/80 backdrop-blur-lg  z-50">
+      <nav className=" max-w-6xl mx-auto flex items-center justify-between px-6 md:px-3 ">
+        <Link href="/">
+          <a className=" w-24 h-16  relative cursor-pointer block">
             <Image
               src={logo}
               layout="fill"
@@ -44,47 +22,63 @@ function Navbar() {
             />
           </a>
         </Link>
-
-        <div className="flex">
-          <div
-            className={` ${
-              isSidebar ? "block " : "hidden"
-            } fixed h-screen lg:h-full  lg:bg-transparent bg-white inset-0  ease-in-out duration-500
-          lg:block lg:relative`}
-          >
-            {/* <div className="relativ"> */}
-            <MdOutlineClose
-              className="cursor-pointer text-3xl  right-10 top-10 absolute lg:hidden "
+        <ul
+          className={`flex lg:p-0 p-5 lg:flex-row justify-center lg:-left-[0vw] transition-all ease-linear duration-200 flex-col lg:relative fixed lg:h-fit h-screen lg:bg-transparent bg-white inset-0 ${
+            isSidebar ? "-left-[0vw]" : "-left-[100vw] "
+          }  lg:w-fit w-full `}
+        >
+          <Link href="/">
+            <a
+              className="lg:hover:bg-transparent hover:bg-zinc-50 transition-all ease-linear duration-100  hover:text-purple-400 flex flex-col items-center  px-4 py-4 rounded lg:py-2 my-6 lg:my-1 font-medium text-md"
               onClick={() => setIsSidebar(false)}
-            />
-            {/* </div> */}
+            >
+              Home
+            </a>
+          </Link>
+          <Link href="#features">
+            <a
+              className="lg:hover:bg-transparent hover:bg-zinc-50 transition-all ease-linear duration-100  hover:text-purple-400 flex flex-col items-center  px-4 py-4 rounded lg:py-2 my-6 lg:my-1 font-medium text-md"
+              onClick={() => setIsSidebar(false)}
+            >
+              Features
+            </a>
+          </Link>
+          <Link href="/api">
+            <a
+              className="lg:hover:bg-transparent hover:bg-zinc-50 transition-all ease-linear duration-100  hover:text-purple-400 flex flex-col items-center  px-4 py-4 rounded lg:py-2 my-6 lg:my-1 font-medium text-md"
+              onClick={() => setIsSidebar(false)}
+            >
+              Api
+            </a>
+          </Link>
+          <Link href="/login">
+            <a
+              className="lg:bg-transparent bg-purple-100 lg:hover:text-purple-400 flex flex-col items-center  px-4 py-4 lg:py-2 mt-4 mb-0 lg:mt-1 lg:mb-1 font-medium text-md"
+              onClick={() => setIsSidebar(false)}
+            >
+              Log In
+            </a>
+          </Link>
+          <Link href="/signup">
+            <a
+              className="bg-purple-400 text-zinc-100 rounded-md flex flex-col items-center  px-4 py-4 lg:py-2 my-6 lg:my-1 font-medium text-md"
+              onClick={() => setIsSidebar(false)}
+            >
+              Sign Up
+            </a>
+          </Link>
 
-            <ul className="flex lg:flex-row ml-auto flex-col justify-center items-center h-full -mt-10 md:-mt-0">
-              {navLinks.map((navLink) => (
-                <li
-                  className="hover:text-purple-400 flex flex-col items-center  px-4 py-2 my-6 lg:my-1 font-medium text-md "
-                  onClick={() => setIsSidebar(false)}
-                  key={navLink.title}
-                >
-                  <Link href={navLink.path}>{navLink.title}</Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-
-          <div className="flex items-center ml-3">
-            {/* {router.pathname === "/tickets" ? null : ( */}
-            <button className="hover:bg-purpe-500 ease-in-out duration-300 bg-purple-400  lg:px-3 px-2 py-2 rounded text-sm font-semibold text-zinc-50">
-              <Link href="/signup">Sign Up</Link>
-            </button>
-            {/* )} */}
-
-            <FiMenu
-              onClick={() => setIsSidebar(true)}
-              className="ml-4 mr-1 lg:hidden cursor-pointer text-2xl"
-            />
-          </div>
-        </div>
+          {/* close mobile menu button */}
+          <MdOutlineClose
+            className="cursor-pointer text-3xl  right-10 top-10 absolute lg:hidden "
+            onClick={() => setIsSidebar(false)}
+          />
+        </ul>{" "}
+        {/* open mobile menu button */}
+        <CgMenu
+          onClick={() => setIsSidebar(true)}
+          className="ml-4 mr-1 lg:hidden cursor-pointer text-3xl"
+        />
       </nav>
     </header>
   );
