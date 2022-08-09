56ea2e1fca691d5f715081ac4ace1615d460b032
Chibuike Nnaji <chibuikennaji306@gmail.com>
2022-08-09 21:50:00 +0100

    Added Upload excel component

--- components/Grupr.tsx
+++ components/Grupr.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import UploadExcel from "./UploadExcel";
 
 function Grupr() {
+  const [isExcel, setisExcel] = useState(false);
+  const [title, setTitle] = useState("");
   return (
     <form className="shadow-md rounded-lg p-2 bg-white flex flex-wrap max-w-xl mt-10 mx-auto border-purple-200/5 border-2">
       <div className="w-full md:w-[75%] p-2 order-2 md:order-none">
         <input
+          onChange={(e) => setTitle(e.target.value)}
           type="text"
           required
           placeholder="Title"
@@ -13,25 +17,34 @@ function Grupr() {
       </div>
       <div className="w-full md:w-[25%] p-2 order-1 md:order-none">
         <button
+          onClick={() => setisExcel(!isExcel)}
           type="button"
-          className="px-2 py-3 outline-none font-semibold  bg-orange-100  rounded-md text-orange-500 w-full"
+          className={`px-2 py-3 outline-none font-semibold    rounded-md ${
+            isExcel
+              ? "text-purple-400 bg-purple-100"
+              : "text-orange-500 bg-orange-100"
+          } w-full`}
         >
-          Import CSV
+          {isExcel ? " Manually" : "Import CSV"}
         </button>
       </div>
 
       <div className="w-full p-2 order-3 md:order-none">
-        <textarea
-          className="p-3 outline-none focus:border-1 focus:border-purple-200 rounded-md bg-purple-100 text-purple-300 w-full"
-          placeholder={
-            "https://www.androidpill.com/\nhttps://dribbble.com/\nhttps://www.activision.com/"
-          }
-          autoFocus={true}
-          required
-          rows={5}
-          //   cols={20}
-          //   wrap="hard"
-        ></textarea>
+        {isExcel ? (
+          <UploadExcel title={title} />
+        ) : (
+          <textarea
+            className="p-3 outline-none focus:border-1 focus:border-purple-200 rounded-md bg-purple-100 text-purple-300 w-full"
+            placeholder={
+              "https://www.androidpill.com/\nhttps://dribbble.com/\nhttps://www.activision.com/"
+            }
+            autoFocus={true}
+            required
+            rows={5}
+            //   cols={20}
+            //   wrap="hard"
+          ></textarea>
+        )}
         <div className="w-full  mt-3 order-4 md:order-none">
           <button
             type="submit"
---
+++ components/UploadExcel.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Excel from "../images/Excel.png";
+import Image from "next/image";
+interface UploadExcelProps {
+  title: string;
+}
+function UploadExcel({ title }: UploadExcelProps) {
+  return (
+    <div className="min-h-[150px] relative cursor-pointer border-dashed border-2 rounded-md flex items-center flex-col">
+      <div className="w-10 h-10 mt-10 mb-2  text-center  rounded-full relative ">
+        <Image
+          src={Excel}
+          alt="link"
+          className="  h-full w-full "
+          layout="fill"
+          priority
+          loading="eager"
+        />
+      </div>
+      <h4 className="text-md font-bold text-zinc-800">
+        Drag and drop files, <span className="text-purple-400">or Browse</span>
+      </h4>
+      <input type="file" className="absolute inset-0 opacity-0 cursor-pointer" />
+    </div>
+  );
+}
+
+export default UploadExcel;
