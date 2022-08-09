1dd8d96ca3430a74be3e19b25c81c2826e743e4c
Chibuike Nnaji <chibuikennaji306@gmail.com>
2022-08-09 18:44:39 +0100

    update newsletter section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,13 +4,12 @@ function Footer() {
   return (
     <footer className="w-full  border-top border-top-2 mt-16">
       <section className="max-w-5xl mx-auto p-3 ">
-        <h2 className="text-4xl font-bold text-purple-400 text-center">
-          Shorten <span className="text-orange-500">multiple links</span>, share
-          as one.
+        <h2 className="text-3xl font-bold text-purple-400 text-center">
+          Subscribe to our Newsletter
         </h2>
         <p className="mt-6 text-md md:text-xl max-w-3xl mx-auto text-center relative text-zinc-600">
-          Reference site about Lorem Ipsum, giving information on its origins,
-          as well as a random Lipsum generator.
+          Stay up to date with th elatest features and releases. Get notified by
+          mail whenever we ship something new and fun.
         </p>
 
         <form className="my-10 p-3 items-center justify-center max-w-3xl mx-auto rounded-lg  flex flex-wrap bg-purple-100 text-purple-300">
