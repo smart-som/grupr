98e5181df620b2b4d3caaafc17f3c9a12f6c88be
Chibuike Nnaji <chibuikennaji306@gmail.com>
2022-08-09 17:05:52 +0100

    added frames

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,14 @@ import link from "../images/link.png";
 import links from "../images/links.png";
 import VulcanSalute from "../images/Vulcan-salute.png";
 import Grupr from "../components/Grupr";
+import frame1 from "../images/frame (1).png";
+import frame2 from "../images/frame (2).png";
+import frame3 from "../images/frame (3).png";
+import frame4 from "../images/frame (4).png";
 const Home: NextPage = () => {
   return (
     // <div className="flex min-h-screen flex-col items-center justify-center py-2">
-    <main className="flex w-full flex-1 flex-col items-center justify-center px-5 text-center max-w-5xl pt-24 mx-auto">
+    <main className="flex w-full flex-1 flex-col items-center justify-center px-5 text-center max-w-5xl pt-24 mx-auto ">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
@@ -70,7 +74,7 @@ const Home: NextPage = () => {
             loading="eager"
           />
         </div>
-        <div className="w-9 h-9  rounded-full absolute right-0 -bottom-64 ">
+        {/* <div className="w-9 h-9  rounded-full absolute right-0 -bottom-64 ">
           <Image
             src={VictoryHand}
             alt="Chart"
@@ -79,7 +83,7 @@ const Home: NextPage = () => {
             priority
             loading="eager"
           />
-        </div>
+        </div> */}
         <div className="w-9 h-9  rounded-full absolute right-10 md:bottom-10 md:block hidden ">
           <Image
             src={links}
@@ -92,6 +96,110 @@ const Home: NextPage = () => {
         </div>
 
         {/************ decor images ************/}
+      </section>
+
+      <section className="py-10  mt-32">
+        {/* frame 1 */}
+        <div className="w-full  flex flex-wrap my-20 py-10">
+          <div className="w-full md:w-1/2 p-3  flex items-center flex-col justify-center">
+            <h2 className="text-3xl font-bold text-purple-400">
+              One short link,
+              <span className="text-orange-500">Infinite possibilities.</span>
+            </h2>
+            <p className="mt-5 font-light text-center lg:mb-0 mb-10">
+              Reference site about Lorem Ipsum, giving information on its
+              origins, as well as a random Lipsum generator.
+            </p>
+          </div>
+          <div className="w-full md:w-1/2 p-5  relative min-h-[450px]">
+            <Image
+              src={frame4}
+              alt="link"
+              className="absolute  h-full w-full "
+              layout="fill"
+              objectFit="contain"
+              priority
+              loading="eager"
+            />
+          </div>
+        </div>
+
+        {/* frame 2*/}
+
+        <div className="w-full  flex lg:flex-row-reverse flex-wrap my-20 py-10">
+          <div className="w-full md:w-1/2 p-3  flex items-center flex-col justify-center">
+            <h2 className="text-3xl font-bold text-purple-400">
+              One short link,
+              <span className="text-orange-500">Infinite possibilities.</span>
+            </h2>
+            <p className="mt-5 font-light text-center lg:mb-0 mb-10">
+              Reference site about Lorem Ipsum, giving information on its
+              origins, as well as a random Lipsum generator.
+            </p>
+          </div>
+          <div className="w-full md:w-1/2 p-5  relative min-h-[450px]">
+            <Image
+              src={frame1}
+              alt="link"
+              className="absolute  h-full w-full "
+              layout="fill"
+              objectFit="contain"
+              priority
+              loading="eager"
+            />
+          </div>
+        </div>
+
+        {/* frame 3 */}
+        <div className="w-full  flex flex-wrap my-20 py-10">
+          <div className="w-full md:w-1/2 p-3  flex items-center flex-col justify-center">
+            <h2 className="text-3xl font-bold text-purple-400">
+              One short link,
+              <span className="text-orange-500">Infinite possibilities.</span>
+            </h2>
+            <p className="mt-5 font-light text-center lg:mb-0 mb-10">
+              Reference site about Lorem Ipsum, giving information on its
+              origins, as well as a random Lipsum generator.
+            </p>
+          </div>
+          <div className="w-full md:w-1/2 p-5  relative min-h-[450px]">
+            <Image
+              src={frame2}
+              alt="link"
+              className="absolute  h-full w-full "
+              layout="fill"
+              objectFit="contain"
+              priority
+              loading="eager"
+            />
+          </div>
+        </div>
+
+        {/* frame 4 */}
+
+        <div className="w-full lg:flex-row-reverse  flex flex-wrap my-20 py-10">
+          <div className="w-full md:w-1/2 p-3  flex items-center flex-col justify-center">
+            <h2 className="text-3xl font-bold text-purple-400">
+              One short link,
+              <span className="text-orange-500">Infinite possibilities.</span>
+            </h2>
+            <p className="mt-5 font-light text-center lg:mb-0 mb-10">
+              Reference site about Lorem Ipsum, giving information on its
+              origins, as well as a random Lipsum generator.
+            </p>
+          </div>
+          <div className="w-full md:w-1/2 p-5  relative min-h-[450px]">
+            <Image
+              src={frame3}
+              alt="link"
+              className="absolute  h-full w-full "
+              layout="fill"
+              objectFit="contain"
+              priority
+              loading="eager"
+            />
+          </div>
+        </div>
       </section>
     </main>
   );
