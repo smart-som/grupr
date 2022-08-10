c50c41ea0d9f32e9a0084072bd04e18cf27a43b0
Chibuike Nnaji <chibuikennaji306@gmail.com>
2022-08-10 23:27:22 +0100

    Update Modal.tsx

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -3,10 +3,10 @@ import { MdOutlineClose } from "react-icons/md";
 import { TModalProps } from "../types";
 function Modal({ setIsModal, children }: TModalProps) {
   return (
-    <section className="z-[100] fixed inset-0 backdrop-blur-lg flex justify-center p-3 bg-white/80 items-center">
+    <section className="z-[100] fixed inset-0 backdrop-blur-xl flex justify-center p-3 bg-white/90 items-center">
       {/* close mobile menu button */}
 
-      <div className=" relative lg:w-[40%] w-[90%] lg:p-5 py-10 p-3 bg-white/70 shadow rounded-md">
+      <div className=" relative lg:w-[40%] w-[90%] lg:p-5 py-10 p-3 bg-white/80 shadow rounded-md">
         <MdOutlineClose
           className="cursor-pointer text-2xl text-zinc-600  right-2 top-2 absolute "
           onClick={() => setIsModal(false)}
