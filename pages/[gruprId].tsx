8ba5486a9dd5d83c7a9b0fde96c80b27732056ce
Chibuike Nnaji <chibuikennaji306@gmail.com>
2022-08-11 09:23:41 +0100

    added api route and general preview page

---
+++ pages/[gruprId].tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Index(props: any) {
+  console.log(props);
+  return <div>Index</div>;
+}
+
+export default Index;
--- pages/api-v1/index.tsx
+++ pages/api-v1/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function index() {
   return (
     <main className="p-3 py-32">
-      <section className="mx-auto max-w-5xl text-center">
+      <section className="mx-auto max-w-5xl text-center ">
         <h1 className="text-5xl font-bold text-purple-400 ">COMING SOON!</h1>
       </section>
     </main>
