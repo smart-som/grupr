e035e93087c2e7f0e7f04d64ec3d3b2af00372a2
Chibuike Nnaji <chibuikennaji306@gmail.com>
2022-08-11 13:39:55 +0100

    Update firebaseAdmin.ts

--- config/firebaseAdmin.ts
+++ config/firebaseAdmin.ts
@@ -3,7 +3,9 @@ import * as firebaseAdmin from "firebase-admin";
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert({
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY
+        ? Buffer.from(process.env.FIREBASE_PRIVATE_KEY).toString()
+        : undefined,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       projectId: "grupr-b9d32",
     }),
