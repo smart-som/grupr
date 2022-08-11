90d8c5f34e999f4a4b889f6ac6e00a51d986c9fe
Chibuike Nnaji <chibuikennaji306@gmail.com>
2022-08-12 00:53:19 +0100

    Update Modal.tsx

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -9,11 +9,11 @@ function Modal({ setIsModal, children, isModal }: TModalProps) {
     }
   }, []);
   useEffect(() => {
-    if (isModal) {
-      document.body.style.overflow = "hidden !important";
-    } else {
-      document.body.style.overflow = "auto";
-    }
+    // if (isModal) {
+    //   document.body.style.overflow = "hidden !important";
+    // } else {
+    //   document.body.style.overflow = "auto";
+    // }
     document.addEventListener("keydown", escFunction);
 
     return () => {
@@ -22,8 +22,8 @@ function Modal({ setIsModal, children, isModal }: TModalProps) {
   });
 
   return (
-    <section className="z-[100] fixed inset-0 backdrop-blur-lg flex justify-center p-3 bg-white/60 items-center">
-      <div className=" relative lg:w-[40%] w-[90%] lg:p-5 py-10 p-3 bg-white/80 shadow rounded-md">
+    <section className="z-[100] fixed inset-0 backdrop-blur-2xl flex justify-center p-3 bg-white/60 items-center">
+      <div className=" relative lg:w-[40%] w-[90%] lg:p-5 py-10 p-3 bg-white shadow rounded-md">
         <MdOutlineClose
           className="cursor-pointer text-2xl text-zinc-600  right-2 top-2 absolute "
           onClick={() => setIsModal(false)}
