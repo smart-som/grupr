6e71dc84e6ab2bc8ae35306b43707dee8b58ad41
Chibuike Nnaji <chibuikennaji306@gmail.com>
2022-08-12 14:37:31 +0100

    update newsletter section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import { BsHeartFill } from "react-icons/bs";
 function Footer() {
   return (
-    <footer className="w-full  border-top border-top-2  md:mt-10">
-      <section className="max-w-5xl mx-auto p-3 ">
-        <h2 className="text-3xl font-bold text-purple-400 text-center">
+    <footer className="w-full  border-top border-top-2  px-3 md:mt-10">
+      <section className="max-w-5xl mx-auto px-3 bg-purple-400 my-5 pb-6 pt-10 rounded-lg">
+        <h2 className="text-3xl font-bold text-purple-100 text-center">
           Subscribe to our Newsletter
         </h2>
-        <p className="mt-6 text-md md:text-xl max-w-3xl mx-auto text-center relative text-zinc-600">
+        <p className="mt-6 text-md md:text-xl max-w-3xl mx-auto text-center relative text-zinc-100">
           Stay up to date with th elatest features and releases. Get notified by
           mail whenever we ship something new and fun.
         </p>
 
-        <form className="my-10 p-3 items-center justify-center max-w-3xl mx-auto rounded-lg  flex flex-wrap bg-purple-100 text-purple-300">
+        <form className="my-10 p-3 items-center  justify-center max-w-3xl mx-auto rounded-lg  flex flex-wrap bg-white/90 text-purple-300">
           <input
             type="email"
             className="md:w-[75%] w-full my-1 bg-transparent  outline-none pl-5 py-4"
