fa9c7af94d83f322e4a6b2a5cd9ed7ff76cb720d
Chibuike Nnaji <chibuikennaji306@gmail.com>
2022-08-12 13:38:07 +0100

    Update Grupr.tsx

--- components/Grupr.tsx
+++ components/Grupr.tsx
@@ -9,7 +9,7 @@ import { handleGruprSubmit } from "../helpers/handleGruprSubmit";
 import CopyToClipboard from "./CopyToClipboard";
 import Share from "./Share";
 import { checkAuth } from "../helpers/checkAuth";
-
+import Link from "next/link";
 function Grupr() {
   const [isExcel, setisExcel] = useState(false);
   const [isModal, setIsModal] = useState(false);
@@ -133,14 +133,11 @@ function Grupr() {
                 <CopyToClipboard type={1} text={grupUrl} />
               </div>
               <div className="w-full p-2 md:w-1/2">
-                <a
-                  href={grupUrl.slice(20, -1)}
-                  target="_blank"
-                  type="button"
-                  className="px-2  py-4 outline-none  lg:my-2 flex gap-x-3  justify-center items-center rounded-md text-zinc-100 font-semibold hover:bg-purple-500 transition-all ease-in-out bg-purple-400  w-full"
-                >
-                  <FiExternalLink className="text-xl" /> Preview
-                </a>
+                <Link href={grupUrl.slice(20, -1)}>
+                  <a className="px-2  py-4 outline-none  lg:my-2 flex gap-x-3  justify-center items-center rounded-md text-zinc-100 font-semibold hover:bg-purple-500 transition-all ease-in-out bg-purple-400  w-full">
+                    <FiExternalLink className="text-xl" /> Preview
+                  </a>
+                </Link>
               </div>
               <Share url={grupUrl} />
             </div>
