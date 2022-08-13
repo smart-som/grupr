658378839cc669e6a61819ee0609193219924fc1
Chibuike Nnaji <chibuikennaji306@gmail.com>
2022-08-13 02:12:53 +0100

    added google analytics measurement  functionality

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,24 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { AuthProvider } from "../context/AuthContext";
 
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as gtag from "../helpers/gtag";
+const isProduction = process.env.NODE_ENV === "production";
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      /* invoke analytics function only for production */
+      if (isProduction) gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+  // eslint-disable-next-line react/jsx-props-no-spreading
   return (
     <>
       <AuthProvider>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,49 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+import { GA_TRACKING_ID } from "../helpers/gtag";
 
+const isProduction = process.env.NODE_ENV === "production";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        {/* enable analytics script only for production */}
+        {isProduction && (
+          <>
+            {/* <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            /> */}
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              strategy="afterInteractive"
+            />
+            {/* <script
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{
+                __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+              }}
+            /> */}
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `}
+            </Script>
+          </>
+        )}
+      </Head>
       <body className="pt-[80px]">
         <Main />
         <NextScript />
